3079155acc96c166138271b226aad12af5357294
Kelly <lykelly2014@icloud.com>
2019-01-20 15:13:35 -0500

    OK Button

--- Gui.py
+++ Gui.py
@@ -1,4 +1,15 @@
-import tkinter
-top = tkinter.Tk()
+# created with python 3
+'''
+Reminder: python 2 ~ from Tkinter import *
+          python 3 ~ from tkinter import *
+'''
+from tkinter import *
+master = Tk()
 
-top.mainloop()
+def callback():
+    print("Click!")
+
+b = Button(master, text="OK", command=callback)
+b.pack()
+
+mainloop()
